1e670e072e6b2bdaaaadd36338c1f749176335e6
LimitToInfinity <21stLimitless@gmail.com>
2019-11-11 12:18:08 -0700

    Made counter app.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Incrementer 
+      :count="count"
+      @increment="increment"
+      @decrement="decrement"
+    />
   </div>
 </template>
+
+<script>
+  import Incrementer from "@/components/Incrementer"
+
+  export default {
+    components: {
+      Incrementer,
+    },
+    data() {
+      return {
+        count: 1
+      }
+    },
+    methods: {
+      increment(amount) {
+        this.count = this.count + amount
+      },
+      decrement(amount) {
+        this.count = this.count - amount
+      }
+    },
+  }
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Incrementer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="incrementer">
+        <p>{{ countString }}</p>
+        <button @click="increment">Increment!</button>
+        <button @click="decrement">Decrement!</button>
+    </div>    
+</template>
+
+<script>
+    export default {
+        props: {
+            count: Number,
+        },
+        computed: {
+            countString() {
+                return `The count is ${this.count}`
+            }
+        },
+        methods: {
+            increment() {
+                this.$emit("increment", 1)
+            },
+            decrement() {
+                this.$emit("decrement", 1)
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
